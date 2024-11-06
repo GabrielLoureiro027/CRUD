fea8c13f7341ff5f2b2b8b1e4a728a3f5f923eff
Gabriel Loureiro <loureirogabriel672@gmail.com>
2024-11-06 11:03:39 -0300

    dasd
    as

--- index.js
+++ index.js
@@ -9,15 +9,12 @@ let idCounter = 1;
 // POST: Criar Produtos
 app.post('/CriarProdutos', (req, res) => {
     const { nome, preco, quantidade } = req.body;
-    if (!produtos.nome || !produtos.preco || !produtos.quantidade) {
-        const novoProduto = { id: idCounter++, nome, preco, quantidade };
-        produtos.push(novoProduto);
-        res.status(201).json(novoProduto);
-        
-    }else{
+    if (nome===null || preco===null || quantidade===null) {
         return res.status(400).json({ message: 'Os campos nome, preco e quantidade são obrigatórios.' });
     }
-
+    const novoProduto = { id: idCounter++, nome, preco, quantidade };
+    produtos.push(novoProduto);
+    res.status(201).json(novoProduto);
 });
 
 // GET: Mostrar Produtos
